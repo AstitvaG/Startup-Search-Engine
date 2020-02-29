a12d12cb7ba8073757e7e98e5e17ac7f81eb0a93
Astitva Gupta <gupta.astitva@gmail.com>
2020-02-29 18:09:02 +0530

    Added school data to tables

--- App/backend/models/table.js
+++ App/backend/models/table.js
@@ -1,38 +1,16 @@
 const mongoose = require('mongoose');
 
 let Table = new mongoose.Schema({
-    sno: {
-        type: Number
+    // sno: {
+    //     type: Number
+    // },
+    title: {
+        type: String,
+        default : null
     },
-    column1: {
-        type:String
-    },
-    column2: {
-        type:String
-    },
-    column3: {
-        type:String
-    },
-    column4: {
-        type:String
-    },
-    column5: {
-        type:String
-    },
-    column6: {
-        type:String
-    },
-    column7: {
-        type:String
-    },
-    column8: {
-        type:String
-    },
-    column9: {
-        type:String
-    },
-    column10: {
-        type:String
+    address: {
+        type: Object,
+        default : null
     }
 });
 
