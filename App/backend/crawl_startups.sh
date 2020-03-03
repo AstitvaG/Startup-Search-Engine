88dfad3bf6e5b608014abbe17cda7343bdd66a00
Sachin Kumar <seethesachin105@gmail.com>
2020-03-03 19:05:21 +0530

    updated script

--- App/backend/crawl_startups.sh
+++ App/backend/crawl_startups.sh
@@ -3,9 +3,9 @@ cd data_crawling
 rm items.json 2> /dev/null
 if test -z "$2"
 then
-    scrapy crawl startupprototype -o items.json -t json -a domain="https://startuptracker.io/discover?filters%5B0%5D%5Bcc%5D%5Bq%5D=$1" --nolog >/dev/null
+    scrapy crawl startupprototype -o items.json -t json -a domain="https://startuptracker.io/discover?filters[0][bm][o]=inc&filters[0][bm][q]=$1" --nolog >/dev/null
 else
-    scrapy crawl startupprototype -o items.json -t json -a domain="https://startuptracker.io/discover?filters%5B0%5D%5Bcc%5D%5Bq%5D=$1" --nolog >/dev/null
+    scrapy crawl startupprototype -o items.json -t json -a domain="https://startuptracker.io/discover?filters[0][bm][o]=inc&filters[0][bm][q]=$1&filters[1][cc][q]=$2" --nolog >/dev/null
 fi
 cd ..
 cat data_crawling/items.json
