a397a08fa58ea68314b257e4c9ce84fda4e03544
Sachin Kumar <seethesachin105@gmail.com>
2020-04-21 16:30:11 +0530

    fixed bugs

--- App/backend/data_crawling/prototpye/spiders/indstup_spider.py
+++ App/backend/data_crawling/prototpye/spiders/indstup_spider.py
@@ -10,8 +10,12 @@ class StackItem(Item):
     description = Field()
     city = Field()
     country = Field()
+    domains = Field()
+    funding_raised = Field()
+    funding_rounds = Field()
+    image = Field()
     foundingdate = Field()
-    # keywords = Field()
+    keywords = Field()
     size_employees = Field()
     twitter = Field()
     facebook = Field()
@@ -49,26 +53,78 @@ class QuotesSpider(scrapy.Spider):
         # print("===========\n\n\n\n")
         # yield item
         # item['keywords'] = j['startupProfile']['data']['detailedInfo']['keywords']
-           
-        item['image'] = j['startupProfile']['data']['image']['url']
-        item['domains'] = j['startupProfile']['data']['detailedInfo']['keywords']
-        item['funding_raised'] = j['startupProfile']['data']['size']['fundingRaised']
-        item['funding_rounds'] = j['startupProfile']['data']['size']['fundingRounds']
-
-        item['name'] = j['startupProfile']['data']['info']['name']
-        item['website'] = j['startupProfile']['data']['info']['website']
-        item['description'] = j['startupProfile']['data']['info']['shortDescription']
-        item['city'] = j['startupProfile']['data']['info']['city']
-        item['country'] = j['startupProfile']['data']['info']['country']
-        item['foundingdate'] = time.strftime('%Y-%m-%d', time.localtime(j['startupProfile']['data']['info']['foundingDate']))
-        item['size_employees'] = j['startupProfile']['data']['size']['employees']
-        item['twitter'] = j['startupProfile']['data']['size']['twitter']
-        item['facebook'] = j['startupProfile']['data']['size']['facebook']
-        item['linkedin'] = j['startupProfile']['data']['size']['linkedin']
-        item['alexarank'] = j['startupProfile']['data']['size']['alexaRank']
-        item['alexaviews'] = j['startupProfile']['data']['size']['alexaPageViews']
-        item['founders'] = j['startupProfile']['data']['founders']
-        item['providers'] = j['startupProfile']['data']['providers']
+        try:
+            item['image'] = j['startupProfile']['data']['image']['url']
+        except:
+            item['image'] = null
+        try:
+            item['domains'] = j['startupProfile']['data']['detailedInfo']['keywords']
+        except:
+            item['domains'] = null
+        try:
+            item['funding_raised'] = j['startupProfile']['data']['size']['fundingRaised']
+        except:
+            item['funding_raised'] = null
+        try:
+            item['funding_rounds'] = j['startupProfile']['data']['size']['fundingRounds']
+        except:
+            item['funding_rounds'] = null
+        try:
+            item['name'] = j['startupProfile']['data']['info']['name']
+        except:
+            item['name'] = null
+        try:
+            item['website'] = j['startupProfile']['data']['info']['website']
+        except:
+            item['website'] = null
+        try:
+            item['description'] = j['startupProfile']['data']['info']['shortDescription']
+        except:
+            item['description'] = null
+        try:
+            item['city'] = j['startupProfile']['data']['info']['city']
+        except:
+            item['city'] = null
+        try:
+            item['country'] = j['startupProfile']['data']['info']['country']
+        except:
+            item['country'] = null
+        try:
+            item['foundingdate'] = time.strftime('%Y-%m-%d', time.localtime(j['startupProfile']['data']['info']['foundingDate']))
+        except:
+            item['foundingdate'] = null
+        try:
+            item['size_employees'] = j['startupProfile']['data']['size']['employees']
+        except:
+            item['size_employees'] = null
+        try:
+            item['twitter'] = j['startupProfile']['data']['size']['twitter']
+        except:
+            item['twitter'] = null
+        try:
+            item['facebook'] = j['startupProfile']['data']['size']['facebook']
+        except:
+            item['facebook'] = null
+        try:
+            item['linkedin'] = j['startupProfile']['data']['size']['linkedin']
+        except:
+            item['linkedin'] = null
+        try:
+            item['alexarank'] = j['startupProfile']['data']['size']['alexaRank']
+        except:
+            item['alexarank'] = null
+        try:
+            item['alexaviews'] = j['startupProfile']['data']['size']['alexaPageViews']
+        except:
+            item['alexaviews'] = null
+        try:
+            item['founders'] = j['startupProfile']['data']['founders']
+        except:
+            item['founders'] = null
+        try:
+            item['providers'] = j['startupProfile']['data']['providers']
+        except:
+            item['providers'] = null
         print(item)
         yield item
         
