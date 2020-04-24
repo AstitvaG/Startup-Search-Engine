1160cb6170da62c2f4b0f463840be069acee7f90
Astitva Gupta <gupta.astitva@gmail.com>
2020-04-24 22:22:04 +0530

    Error handled

--- App/frontend/src/components/navbar.component.js
+++ App/frontend/src/components/navbar.component.js
@@ -7,6 +7,7 @@ export default class Navbar extends Component {
     constructor(props) {
         super(props);
         this.logout = this.logout.bind(this);
+        if (window.localStorage.getItem('name') == "null") this.logout();
     }
 
     logout() {
